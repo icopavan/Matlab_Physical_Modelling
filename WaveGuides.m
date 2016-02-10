7b29a30576de4b106b0abaaf0a66b0743a999993
rampartisan <alexbaldwinaudio@gmail.com>
2016-02-10 10:40:12 +0100

    PhasFix

--- WaveGuides.m
+++ WaveGuides.m
@@ -3,17 +3,19 @@
 % Sample Rate
 Fs = 44100;
 % String Freq
-Fc = 216;
+Fc = 108;
 % Total delay line length
 N = floor(Fs/Fc);
 % Pluck Pos
-Pp = 0.1;
+Pp = 0.8;
 NPp = floor(N * Pp);
 % PickUp Pos
 Pu = 0.2;
 NPu = floor(N * Pu);
 % init left and right delays with triangle input, centre at pluck pos
-[yl,yr] = deal(conv([[0:Pp]/Pp,(N-[(Pp+1):N])/(N-Pp)],x,'same'));
+% [yl,yr,in] = deal(conv([[0:NPp]/NPp,(N-[(NPp+1):N])/(N-NPp)],x,'same'));
+[yl,yr,in] = deal(([[0:NPp]/NPp,(N-[(NPp+1):N])/(N-NPp)]));
+
 % init output
 y = yl(NPu) + yr(NPu);
 
@@ -30,18 +32,18 @@ g = 0.1;
 
 while v
     % lpf + apf
-    lpr = LC*yl(end-floor(N/2)) + LC*yl(end-floor(N/2)-1);
-    lpl = LC*yr(end-floor(N/2)) + LC*yr(end-floor(N/2)+1);
+    lpr = LC*yl(end-floor(N/2)) + LC*yl(end-floor(N/2)+1);
+    lpl = LC*yr(end-floor(N/2)) + LC*yr(end-floor(N/2)-1);
     
     % Apf
-     apr = -(g*yl(end-floor(N/2))) + yl(end-floor(N/2)-1) ...
-         + g*yl(end-floor(N/2)-1);
+     apr = -(g*yl(end-floor(N/2))) + yl(end-floor(N/2)+1) ...
+         + g*yl(end-floor(N/2)+1);
      apl = -(g*yr(end-floor(N/2))) + yr(end-floor(N/2)-1) ...
          + g*yr(end-floor(N/2)-1);
    
     % sum of filters
-    yr(end) = (lpr + apr) * 0.5;
-    yl(end) = (lpl + lpl) * 0.5;
+    yr(end) = (-lpr + -apr) * 0.5;
+    yl(end) = (-lpl + -apl) * 0.5;
     
     % shift wave left or right
     yr = circshift(yr,[0 1]);
@@ -60,6 +62,19 @@ end
 i = i+1;
 
 end
+
+
+figure;
+subplot(2,1,1);
+plot(in);
+title('Input - Triangle convolved with body IR');
+xlabel('Time');
+ylabel('Amp');
+subplot(2,1,2);
+plot(y);
+title('Output');
+xlabel('Time');
+ylabel('Amp');
 
 
 soundsc(y,Fs); 
